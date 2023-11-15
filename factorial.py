2a7c9f614bbbe3f63c5d6a78f935ae5cd7c5a114
othmenbelgacem <othmanbelgacem8@gmail.com>
2023-11-15 11:28:01 +0100

    first change test

---
+++ factorial.py
@@ -0,0 +1,23 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def main():
+    try:
+        # Get user o input for the number
+        num = int(input("Enter a non-negative integer: "))
+
+        # Check if the input is non-negative
+        if num < 0:
+            print("Please enter a non-negative integer.")
+        else:
+            result = factorial(num)
+            print("The factorial of {} is: {}".format(num, result))
+
+    except ValueError:
+        print("Invalid input. Please enter a valid non-negative integer.")
+
+if __name__ == "__main__":
+    main()
